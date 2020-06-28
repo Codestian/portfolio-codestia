6c7087edd1fdabe1def47785537e706325d90598
Muhd Hakim <mh.silvine@gmail.com>
2020-06-28 21:21:08 +0800

    Added portfolio stuff

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="page">
     <section class="section">
+      <h1>work in progress</h1>
+
+      <h2>Home Assistant Land Transport Authority Integration</h2>
+      <p>Integration of LTA's datamall API to Home Assistant, an open source smart home autmation platform. Services such as bus timings can be integrated with smart home.</p>
+
+      <h2>Zephyr Music Web Application</h2>
+      <p>A vue based Progressive Web Application that replicates Apple Music and consumes the Spotify API.</p>
 
     </section>
     <section class="section">
@@ -14,5 +21,11 @@
   width: 100%;
   height: 100%;
   background: #212121;
+
+  color: white;
+
+  h2 {
+    padding-top: 32px;
+  }
 }
 </style>
